947235345c9f9f1997d19ced5d02edf3d2a818ef
Ranferi Dominguez <ranferi.dominguez@schibsted.com.mx>
2016-10-15 08:20:35 -0500

    first class primitive objects and iterators

--- iOSBasic/iOSBasic/ViewController.m
+++ iOSBasic/iOSBasic/ViewController.m
@@ -16,10 +16,59 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-    NSLog(@" Hello World ");
+    NSString *myString = @"Hola Mundo";
+    NSLog(@"My Log: %@",myString);
+    
+    NSArray *myArr = @[@"hola",@"como",@"estás"];
+    NSLog(@"My Array: %@",[myArr objectAtIndex:0]);
+    
+    NSString *greetingString = [NSString stringWithFormat:@"%@ %@ %@",myArr[0],myArr[1],myArr[2]];
+    
+    NSDictionary *myDictionary = @{
+                                   @"Key 1" : @"El coche de Jony",
+                                   @"Key 2" : @"El coche de Luis",
+                                   @"Key 3" : @"El coche de Cesar",
+                                   @"Key 4" : @"El coche de Kevin",
+                                   };
+    
+    NSLog(@"DictionaryKey %@",[myDictionary allKeys]);
+    NSLog(@"Dict info %@",myDictionary[@"Key 1"]);
+    
+    NSMutableArray *arrMutable = [[NSMutableArray alloc]initWithArray:myArr];
+    [arrMutable addObject:@"Other object"];
+    [arrMutable addObject:@"Last Object"];
+    [arrMutable removeLastObject];
+    NSLog(@"mutable Array: %@",arrMutable);
+    
+    NSMutableDictionary *dictMutable = [[NSMutableDictionary alloc]initWithDictionary:myDictionary];
+    [dictMutable setValue:@"new value" forKey:@"key 500"];
+    [dictMutable removeObjectForKey:@"key 500"];
+    [dictMutable setValue:myArr forKey:@"array"];
+    
+    [self addUserToArrayWithName:@"Ranferi" andPhone:@"123456"];
+    
+    NSArray *usersNameArray = @[@"Juan", @"Paco", @"Pedro", @"Luis"];
+    NSArray *usersPhoneArray = @[@"5524534", @"3456354", @"356564", @"3456345"];
+    
+    NSMutableArray *userArr = [[NSMutableArray alloc]init];
+    
+    for (id obj in usersNameArray){
+        int i = [usersNameArray indexOfObject:obj];
+        [userArr addObject:[self addUserToArrayWithName:usersNameArray[i] andPhone:usersPhoneArray[i]]];
+    }
+    
+    NSLog(@"user array: %@",userArr);
 }
 
+- (NSDictionary *)addUserToArrayWithName:(NSString *)name andPhone:(NSString *)phone {
+    
+    NSDictionary *userInfoDict = @{
+                                   @"name":name,
+                                   @"phone":phone
+                                   };
+    return userInfoDict;
+    /* Al termino de a ejecución se requiere un arreglo de diccionarios con la información de los usuarios */
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
